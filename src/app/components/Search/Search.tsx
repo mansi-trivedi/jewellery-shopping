66d183833f619f2953857cef4eb2f5905f3c74a9
nammydesigns <nammydesigns@gmail.com>
2025-04-10 17:51:57 +0530

    update search component

--- src/app/components/Search/Search.tsx
+++ src/app/components/Search/Search.tsx
@@ -1,43 +1,42 @@
 import { useRouter } from "next/router";
-import React, { useCallback, useState, ChangeEvent } from "react";
+import React, { useCallback, FormEvent } from "react";
 import toast from "react-hot-toast";
 import { ImSearch } from "react-icons/im";
 
 const Search = () => {
-  const [searchQuery, setSearchQuery] = useState<string>("");
   const router = useRouter();
 
-  const handleOnInputChange = useCallback(
-    (event: ChangeEvent<HTMLInputElement>) => {
-      setSearchQuery(event.target.value);
+  const handleOnSubmit = useCallback(
+    (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      const formData = new FormData(e.currentTarget);
+      const searchQuery = formData.get("search-query") as string;
+      if (!searchQuery.trim()) {
+        toast.error("Please add search term to search");
+        return;
+      }
+      router.push({
+        pathname: "/search",
+        query: { search_term: searchQuery },
+      });
     },
-    []
+    [router]
   );
-
-  const handleOnSearchBtnClick = useCallback(() => {
-    if (!searchQuery.trim()) {
-      toast.error("Please add search term to search");
-      return;
-    }
-    router.push({
-      pathname: "/search",
-      query: { search_term: searchQuery },
-    });
-  }, [searchQuery, router]);
 
   return (
     <div className="w-full border rounded flex justify-center items-center">
       <div className="w-full flex justify-center items-center">
-        <input
-          type="text"
-          placeholder="Search..."
-          className="w-full h-full bg-badgeShade px-2 min-h-8 border-none border-r-0 font-quickSand"
-          value={searchQuery}
-          onChange={handleOnInputChange}
-        />
-        <button className="bg-darkGreen p-2" onClick={handleOnSearchBtnClick}>
-          <ImSearch color="white" />
-        </button>
+        <form onSubmit={handleOnSubmit} className="flex w-full">
+          <input
+            type="text"
+            placeholder="Search..."
+            name="search-query"
+            className="w-full h-full bg-badgeShade px-2 min-h-8 border-none border-r-0 font-quickSand"
+          />
+          <button type="submit" className="bg-darkGreen p-2">
+            <ImSearch color="white" />
+          </button>
+        </form>
       </div>
     </div>
   );
